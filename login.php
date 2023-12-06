b9fd3960794370ff69b30c597f90a171b1218357
digibite <102049297+irwingmovehq@users.noreply.github.com>
2023-12-06 13:58:07 -0800

    Remove unnecessary comments

--- login.php
+++ login.php
@@ -10,8 +10,6 @@ Session::checkLogin(); // Checking whether user is logged in or not.
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- I found this font in the MoveHQ website. Browser developer tools are the best! Points for attention to 
-    detail? lol Just kidding -->
     <!-- <link rel="stylesheet" type="text/css"
         href="https://fonts.googleapis.com/css2?family=Fjalla+One:wght@400&amp;family=Libre+Franklin:wght@400;500"> -->
     <link rel="stylesheet" href="./stylesheets/login.css">
@@ -20,9 +18,6 @@ Session::checkLogin(); // Checking whether user is logged in or not.
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap" rel="stylesheet">
     
 
-    <!-- In production I normally download the library files or would opt for using a faster library like 
-    React for example. 
-    I love jQuery's simplicity and reliability though. -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>
     <title>Document</title>
 </head>
@@ -30,7 +25,6 @@ Session::checkLogin(); // Checking whether user is logged in or not.
     <!-- Simple container to center the content -->
     <div id="main-container">
     <div id="content-container">
-        <!-- <h2>User Login</h2> old login text-->
         <h3>Sign in</h3>
         <p id="text"></p>
         <p id="message"></p>
